a2efc1785eb0c5133a0827f5951a093aa558deb3
Ruan Vinícius <contatoruanvinicius@gmail.com>
2018-10-21 13:42:31 -0300

    adicionando mais rotas

--- back-end/app/routes.php
+++ back-end/app/routes.php
@@ -1,4 +1,11 @@
 <?php
-
+//busca de clientes
 $router->get("ceagro/back-end/clientes", "ClientesController@index");
+//cadastro de clientes
 $router->post("ceagro/back-end/clientes", "ClientesController@cadastrar");
+
+$router->get("ceagro/back-end/produtos", "ProdutosController@index");
+$router->post("ceagro/back-end/produtos", "ProdutosController@cadastrar");
+
+$router->get("ceagro/back-end/contratros", "ContratosController@index");
+$router->post("ceagro/back-end/contratros", "ContratosController@cadastrar");

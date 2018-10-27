f113e5e3d15b051b02ec24f5befb6777a5befe5d
Ruan Vinícius <contatoruanvinicius@gmail.com>
2018-10-27 13:48:22 -0300

    Mudando o htaccess, fazendo uris compostas funcionar'

--- back-end/index.php
+++ back-end/index.php
@@ -6,11 +6,9 @@ require "core/bootstrap.php";
 use App\Core\Request;
 use App\Core\Router;
 
-dd($_SERVER);
-
 try {
     Router::carregar('app/routes.php')->direcionar(
-        dd(Request::uri()),
+        Request::uri(),
         Request::method()
     );
 } catch (\Exception $e) {
